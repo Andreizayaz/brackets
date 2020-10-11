5263c5dfe45e21dcf1897a2c94d15030897a6284
Andreizayaz <poznavauka@mail.ru>
2020-10-11 20:03:34 +0300

    feat: solve task brackets.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketPair = '';
+   for(let i = 0; i<bracketsConfig.length;){
+      bracketPair = bracketsConfig[i].join('');
+     if(str.includes(bracketPair)){
+       str = str.replace(bracketPair,'');
+       i=0;
+     }else{
+       i++;
+     }
+   }
+  
+  return str.length===0;
 }
